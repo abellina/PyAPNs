1b860676c62a988e7cd0dd344787b6e4b543ccc8
Alessandro Bellina <abellina@gmail.com>
2014-04-19 20:03:04 -0600

    Changes for python 3

--- apns.py
+++ apns.py
@@ -134,23 +134,24 @@ class APNsConnection(object):
         # Establish an SSL connection
         
         # Fallback for socket timeout.
-        for i in xrange(3):
+        for i in range(3):
             try:
                 self._socket = socket(AF_INET, SOCK_STREAM)
                 self._socket.settimeout(self.timeout)
                 self._socket.connect((self.server, self.port))
                 break
             except timeout:
+                print ("timeout connecting")
                 pass
             except:
                 raise
 
         # Fallback for 'SSLError: _ssl.c:489: The handshake operation timed out'
-        for i in xrange(3):
+        for i in range(3):
             try:
                 self._ssl = wrap_socket(self._socket, self.key_file, self.cert_file)
                 break
-            except SSLError, ex:
+            except SSLError as ex:
                 if ex.args[0] == SSL_ERROR_WANT_READ:
                     sys.exc_clear()
                 elif ex.args[0] == SSL_ERROR_WANT_WRITE:
